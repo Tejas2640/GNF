c24266c13d5bf20a934ea35b98d9ab4d3992be7c
Tejas2640 <tejasjangra33@gmail.com>
2026-06-12 22:14:37 +0530

    update axios baseURL for production

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gnf-1.onrender.com/api",
+  baseURL: "http://gnf-git-main-tjs03.vercel.app/api",
 });
 
 export default api;
